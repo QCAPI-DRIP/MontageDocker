f7397ed6b70ca880238728f9ae8da16a1e2e415b
skoulouzis <S.Koulouzis@uva.nl>
2019-06-11 14:50:50 +0200

    added taverna workfllows

--- Montage/downloadDSS.sh
+++ Montage/downloadDSS.sh
@@ -3,14 +3,19 @@
 # Pleiades Image creation BASH script.
 # Inseok Song, 2007
 
-MONTAGE_HOME=/root/Montage
-MONTAGE_BIN=$MONTAGE_HOME/bin
-
 for bands in DSS2B DSS2R DSS2IR; do echo Processing ${bands};
-mkdir $bands;
-cd $bands;
-mkdir raw projected;
-cd raw;
-$MONTAGE_BIN/mArchiveList dss ${bands} "56.5 23.75" 3 3 remote.tbl;
-$MONTAGE_BIN/mArchiveExec remote.tbl;
+    mkdir $bands;
+    cd $bands;
+    mkdir raw projected;
+    cd raw;
+    mArchiveList dss ${bands} "56.5 23.75" 3 3 remote.tbl;
+    mArchiveExec remote.tbl;
+    cd .. ;
+#     mImgtbl raw rimages.tbl ;
+#     mProjExec -p raw rimages.tbl ../pleiades.hdr projected stats.tbl ;
+#     mImgtbl projected pimages.tbl ;
+#     mAdd -p projected pimages.tbl ../pleiades.hdr ${bands}.fits ;
+    cd .. ;
 done
+
+    
